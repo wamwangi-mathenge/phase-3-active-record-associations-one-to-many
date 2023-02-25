44b84e1c45896dd76b678d62be1f4b8d0928828b
Brian <wamwangimathenge@gmail.com>
2023-02-25 12:33:51 +0300

    Completed assignment

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,9 @@
 class Game < ActiveRecord::Base
+
+    # def reviews
+    #     Review.where(game_id: self.id)
+    # end
+
+    has_many :reviews
   
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ActiveRecord::Base
+
+    # def game
+    #     # self is the review instance
+    #     Game.find(self.game_id)
+    # end
+
+    belongs_to :game
   
 end
